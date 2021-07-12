fbe4cf8316786c014fb5b1e8a6932d7d10404384
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-12 20:54:19 +0400

    init

---
+++ mcs-data/reting-data-service/src/main/java/com/spring/mcs/lvl/one/retingdataservice/RetingDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mcs.lvl.one.retingdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetingDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetingDataServiceApplication.class, args);
+	}
+
+}
---
+++ mcs-data/reting-data-service/src/test/java/com/spring/mcs/lvl/one/retingdataservice/RetingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mcs.lvl.one.retingdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
