c86a4c8f534a97e9e8089bbe773d748ed860024a
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-12 21:18:50 +0400

    lesson 7 complete, catalog updated

---
+++ mcs-catalog/movie-catalog-service/src/main/java/com/spring/mcs/lvl/one/moviecatalogservice/model/CatalogItem.java
@@ -0,0 +1,42 @@
+package com.spring.mcs.lvl.one.moviecatalogservice.model;
+
+public class CatalogItem {
+
+    private String title;
+    private String description;
+    private int rating;
+
+    public CatalogItem() {
+    }
+
+    public CatalogItem(String title, String description, int rating) {
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+}
