206a738ffa18872908c1b913044c4adeef1b68a5
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-13 21:18:44 +0400

    movie info simple

---
+++ mcs-info/movie-info-service/src/main/java/com/spring/mcs/lvl/one/movieinfoservice/resource/MovieResource.java
@@ -0,0 +1,18 @@
+package com.spring.mcs.lvl.one.movieinfoservice.resource;
+
+import com.spring.mcs.lvl.one.movieinfoservice.model.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "name");
+    }
+
+}
