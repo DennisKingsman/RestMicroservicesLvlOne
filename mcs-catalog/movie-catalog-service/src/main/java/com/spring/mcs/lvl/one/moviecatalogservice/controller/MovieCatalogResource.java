8e5f6477b19e0101241a5cfd33afc6c3fc94d50d
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-15 03:54:45 +0400

    alternative webClient reactive way

--- mcs-catalog/movie-catalog-service/src/main/java/com/spring/mcs/lvl/one/moviecatalogservice/controller/MovieCatalogResource.java
+++ mcs-catalog/movie-catalog-service/src/main/java/com/spring/mcs/lvl/one/moviecatalogservice/controller/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +20,12 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
 
     private RestTemplate restTemplate;
+    private WebClient.Builder webClientBuilder;
+
+    @Autowired
+    public void setWebClientBuilder(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
 
     @Autowired
     public void setRestTemplate(RestTemplate restTemplate) {
@@ -33,6 +40,16 @@ public class MovieCatalogResource {
         );
         return ratings.stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+            /*Alternative WebClient way
+            Movie movie =webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+            */
+
             return new CatalogItem(movie.getName(), "desc", rating.getMovieRating());
         }).collect(Collectors.toList());
     }
